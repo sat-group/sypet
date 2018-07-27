becdc740ad2523e4f4f908d19bcbaef8654c5111
Ruben Martins <rmartins@cs.utexas.edu>
2018-07-27 17:06:15 -0400

    feat: changed the refactor example to include adding days, months, years

--- src/cmu/edu/ui/RefactorExample.java
+++ src/cmu/edu/ui/RefactorExample.java
@@ -19,17 +19,41 @@ public class RefactorExample {
 		ArrayList<String> libs = new ArrayList<String>(Arrays.asList("./lib/rt8.jar"));
 		UISyPet sypet = new UISyPet(packages, libs);
 		String methodName = "refactor";
-		ArrayList<String> paramNames = new ArrayList<String>(Arrays.asList("sypet_arg0", "sypet_arg1", "sypet_arg2"));
+		ArrayList<String> paramNames = new ArrayList<String>(Arrays.asList("sypet_arg0", "sypet_arg1", "sypet_arg2", "sypet_arg3", "sypet_arg4"));
+//		
+//		ArrayList<String> srcTypes = new ArrayList<String>(
+//				Arrays.asList("int","int","int"));
+//		
+//		String testCode = "	public static boolean test0(java.util.Date date0) throws Throwable{\n" + 
+//				"        int arg0 = date0.getYear();\n" + 
+//				"        int arg1 = date0.getDate();\n" + 
+//				"        int arg2 = date0.getMonth();\n" + 
+//				"        java.time.OffsetDateTime offsetdatetime0 = refactor(arg0, arg1, arg2);\n" + 
+//				"        boolean b0 = offsetdatetime0.getYear() == arg0;\n" + 
+//				"        boolean b1 = offsetdatetime0.getDayOfMonth() == arg1;\n" + 
+//				"        boolean b2 = offsetdatetime0.getMonthValue() == arg2;\n" + 
+//				"        return b0 && b1 && b2;\n" + 
+//				"    }\n" + 
+//				" public static boolean test() throws Throwable{\n" +
+//				"        java.util.Date date0 = new java.util.Date(525, 525, 0);\n"+
+//				"        java.util.Date date1 = new java.util.Date(1, 1, 1);\n"+
+//				"        java.util.Date date2 = new java.util.Date(0, 0, 0);\n"+
+//				"        java.util.Date date3 = new java.util.Date((-2934), (-2934), (-2934));\n"+
+//				"        java.util.Date date4 = new java.util.Date(0, 1226, 1226);\n"+
+//				"   return test0(date0) && test0(date1) && test0(date2) && test0(date3) && test0(date4);}\n";
+//		
+//		
+//		String tgtType = "java.time.OffsetDateTime";
 
 		ArrayList<String> srcTypes = new ArrayList<String>(
-				Arrays.asList("java.lang.String", "java.lang.String", "long"));
+				Arrays.asList("java.lang.String", "java.lang.String", "long", "long", "long"));
 
 		String tgtType = "java.lang.String";
 		
 		String testCode = "public static boolean test0() throws Throwable {\n"+
-			"return (refactor(\"2015/10/21\", \"yyyy/MM/dd\",5).equals(\"2015/10/26\"));}\n"+
+			"return (refactor(\"2015/10/21\", \"yyyy/MM/dd\",5,1,2).equals(\"2017/11/26\"));}\n"+
 			"public static boolean test1() throws Throwable {\n"+
-			"return (refactor(\"2013/04/28\", \"yyyy/MM/dd\",7).equals(\"2013/05/05\"));}\n"+
+			"return (refactor(\"2013/04/28\", \"yyyy/MM/dd\",7,2,3).equals(\"2016/07/05\"));}\n"+
 			"public static boolean test() throws Throwable {\n"+
 			"return test0() && test1();}\n";
 
@@ -49,11 +73,11 @@ public class RefactorExample {
 			sypet.addAtLeastK("java.time.LocalDate.plusDays(java.time.LocalDate long )java.time.LocalDate", 1);
 		}
 		
-		//sypet.addAtLeastK("(static)java.time.format.DateTimeFormatter.ofPattern(java.lang.String )java.time.format.DateTimeFormatter",1);
-		//sypet.addAtLeastK("(static)java.time.LocalDate.parse(java.lang.CharSequence java.time.format.DateTimeFormatter )java.time.LocalDatePoly:(java.lang.String java.time.format.DateTimeFormatter )", 1);
-		//sypet.addAtLeastK("java.time.LocalDate.plusDays(java.time.LocalDate long )java.time.LocalDate", 1);
-		//sypet.addAtLeastK("java.time.LocalDate.format(java.time.LocalDate java.time.format.DateTimeFormatter )java.lang.String", 1);
-		String code = sypet.synthesize(5);
+//		sypet.addAtLeastK("(static)java.time.format.DateTimeFormatter.ofPattern(java.lang.String )java.time.format.DateTimeFormatter",1);
+//		sypet.addAtLeastK("(static)java.time.LocalDate.parse(java.lang.CharSequence java.time.format.DateTimeFormatter )java.time.LocalDatePoly:(java.lang.String java.time.format.DateTimeFormatter )", 1);
+//		sypet.addAtLeastK("java.time.LocalDate.plusDays(java.time.LocalDate long )java.time.LocalDate", 1);
+//		sypet.addAtLeastK("java.time.LocalDate.format(java.time.LocalDate java.time.format.DateTimeFormatter )java.lang.String", 1);
+		String code = sypet.synthesize(7);
 		System.out.println("code = " + code);
 
 	}
